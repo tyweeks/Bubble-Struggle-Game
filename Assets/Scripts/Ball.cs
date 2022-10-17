9b3b1710c4d19541e60a031fa137f3c04764b778
Ty Weeks <ty.weeks10@gmail.com>
2022-10-17 15:46:31 -0700

    Split the ball when hit.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,11 +4,28 @@ public class Ball : MonoBehaviour
 {
     public Vector2 startForce;
 
+    public GameObject nextBall;
+
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb.AddForce(startForce, ForceMode2D.Impulse);
+    }
+
+    public void Split()
+    {
+        if (nextBall != null)
+        {
+            GameObject rightBall = Instantiate(nextBall, rb.position + Vector2.right / 4, Quaternion.identity);
+            GameObject leftBall = Instantiate(nextBall, rb.position + Vector2.left / 4, Quaternion.identity);
+
+            rightBall.GetComponent<Ball>().startForce = new Vector2(2f, 5f);
+            leftBall.GetComponent<Ball>().startForce = new Vector2(-2f, 5f);
+
+        }
+
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/ChainCollision.cs
+++ Assets/Scripts/ChainCollision.cs
@@ -5,5 +5,10 @@ public class ChainCollision : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Chain.IsFired = false;
+    
+        if (collision.tag == "Ball")
+        {
+            collision.GetComponent<Ball>().Split();
+        }
     }
 }
